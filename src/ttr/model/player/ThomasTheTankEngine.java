a4b270a457f2d0cc69ba1e4a7204cfbff24bd719
tahiyasalam <ts7ft@virginia.edu>
2015-10-15 21:32:03 -0400

    updates

--- src/ttr/model/player/ThomasTheTankEngine.java
+++ src/ttr/model/player/ThomasTheTankEngine.java
@@ -1,6 +1,7 @@
 package ttr.model.player;
 
 import java.util.ArrayList;
+import ttr.model.destinationCards.*;
 import java.util.HashMap;
 
 import ttr.model.destinationCards.Destination;
@@ -28,27 +29,22 @@ public class ThomasTheTankEngine extends Player {
 		
 	}
 	
-	public ArrayList<Destination> shortestPath(Destination start, Destination end) {
-		ArrayList<Destination> path = new ArrayList<Destination>();
-		
+	public int shortestPathcost(Destination from, Destination to){
 		/* If same, just return false */
-		if(start == end) return path;
+		if(from == to) return 0;
 		
 		/* Open and Closed lists (breadth first search) */
 		HashMap<Destination, Integer> openList = new HashMap<Destination, Integer>();
 		HashMap<Destination, Integer> closedList = new HashMap<Destination, Integer>();
+		HashMap<Destination, Integer> path = new HashMap<Destination, Integer>();
 		
-		int minCost = 9999;
-
-		for(Destination dest : Destination.values()) {
-			openList.put(dest, minCost);
-		}
-		openList.put(start, 0);
+		openList.put(from, 0);
 		
 		while(openList.size() > 0){
 			
 			/* Pop something off the open list, if destination then return true */
 			Destination next = null;
+			int minCost = 9999;
 			for(Destination key : openList.keySet()){
 				if(openList.get(key) < minCost){
 					next = key;
@@ -62,28 +58,31 @@ public class ThomasTheTankEngine extends Player {
 			closedList.put(next, minCost);
 			
 			/* If this is the destination, then return!!!! */
-			if(next == end) return closedList.get(next);
+			if(next == to) return closedList.get(next);
 			
 			/* Get all the neighbors of the next city that aren't on open or closed lists already */
-			for(Destination neighbor : getNeighbors(next)){
+			for(Destination neighbor : Routes.getInstance().getNeighbors(next)){
 				if(closedList.containsKey(neighbor)) continue;
 				
 				/* get route between next and neighbor and see if better than neighbor's value */
-				ArrayList<Route> routesToNeighbor = this.getRoutes(next, neighbor);
+				ArrayList<Route> routesToNeighbor = Routes.getInstance().getRoutes(next, neighbor);
 				for(Route routeToNeighbor : routesToNeighbor){
 					int newCost = closedList.get(next) + routeToNeighbor.getCost();
 					
 					if(openList.containsKey(neighbor)){	
 						if(newCost < openList.get(neighbor)){
 							openList.put(neighbor, newCost);
+							path.put(neighbor, newCost);
 						}
 					}
 					else{
 						openList.put(neighbor, newCost);
+						path.put(neighbor, newCost);
 					}
 				}
 			}
+		}
 		
-		return path;
+		return 0;
 	}
 }
