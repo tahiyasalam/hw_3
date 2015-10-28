89b8d78da34166f5565e1850b5ec5e4f30011450
tahiyasalam <ts7ft@virginia.edu>
2015-10-28 10:28:15 -0400

    added preliminary quality, reward, and value functions

--- src/ttr/model/player/ManifestDestinyPlayer.java
+++ src/ttr/model/player/ManifestDestinyPlayer.java
@@ -19,6 +19,67 @@ public class ManifestDestinyPlayer extends Player {
 	public ManifestDestinyPlayer() {
 		super("ManifestDestinyPlayer");
 	}
+	
+	//STATES:					ACTIONS:
+	//0 = enough cards 			a1 = claim routes
+	//1 = few cards				a2_r = pick random card 
+	//2 = many good				a2_k = pick known card 
+	//3 = many bad				a3 = pick destination
+
+	//matrices for probabilities upon transition
+	double prob_a1[][] = new double[][]{
+		{0.1, 0, 0, 0},
+		{0.5, 1, 0, 0},
+		{0.2, 0, 0, 0},
+		{0.2, 0, 0, 0},
+	};
+	double prob_a2_r[][] = new double[][]{
+		{1, 0.1, 0.5, 0.02},
+		{0, 0.5, 0, 0},
+		{0, 0.2, 0.5, 0.2},
+		{0, 0.2, 0, 0.78},
+	};
+	double prob_a2_k[][] = new double[][]{
+		{1, 0.15, 0.6, 0.05},
+		{0, 0.5, 0, 0},
+		{0, 0.18, 0.4, 0.25},
+		{0, 0.17, 0, 0.7},
+	};
+	double prob_a3[][] = new double[][]{
+		{1, 0, 0.5, 0.33},
+		{0, 1, 0, 0},
+		{0, 0, 0.5, 0.34},
+		{0, 0, 0, 0.33},
+	};
+
+	
+	//matrices for rewards upon transition
+	double rew_a1[][] = new double[][]{
+		{10, 0, 0, 0},
+		{5, 3, 0, 0},
+		{8, 0, 0, 0},
+		{5, 0, 0, 0},
+	};
+	double rew_a2_r[][] = new double[][]{
+		{5, 7, 10, 8},
+		{0, 2, 0, 0},
+		{0, 5, 7, 6},
+		{0, 2, 0, 2},
+	};
+	double rew_a2_k[][] = new double[][]{
+		{5, 7, 10, 8},
+		{0, 2, 0, 0},
+		{0, 5, 7, 6},
+		{0, 2, 2, 2},
+	};
+	double rew_a3[][] = new double[][]{
+		{3, 0, 5, 6},
+		{0, -1, 0, 0},
+		{0, 0, 1, 2},
+		{0, 0, 0, -1},
+	};
+	
+	double gamma = 0.5;
 
 	/**
 	 * MUST override the makeMove() method and implement it.
@@ -46,79 +107,20 @@ public class ManifestDestinyPlayer extends Player {
 		
 		for (int i = 0; i < destTickets.size(); i++) { //sum value of all destination tickets
 			costOfTickets += destTickets.get(i).getValue();
-		}
+		}	
 		
-		//STATES:					ACTIONS:
-		//0 = enough cards 			a1 = claim routes
-		//1 = few cards				a2_r = pick random card 
-		//2 = many good				a2_k = pick known card 
-		//3 = many bad				a3 = pick destination
-		
-		double gamma = 0.5;
-		
-		//matrices for probabilities upon transition
-		double prob_a1[][] = new double[][]{
-			{0.1, 0, 0, 0},
-			{0.5, 1, 0, 0},
-			{0.2, 0, 0, 0},
-			{0.2, 0, 0, 0},
-		};
-		double prob_a2_r[][] = new double[][]{
-			{1, 0.1, 0.5, 0.02},
-			{0, 0.5, 0, 0},
-			{0, 0.2, 0.5, 0.2},
-			{0, 0.2, 0, 0.78},
-		};
-		double prob_a2_k[][] = new double[][]{
-			{1, 0.15, 0.6, 0.05},
-			{0, 0.5, 0, 0},
-			{0, 0.18, 0.4, 0.25},
-			{0, 0.17, 0, 0.7},
-		};
-		double prob_a3[][] = new double[][]{
-			{1, 0, 0.5, 0.33},
-			{0, 1, 0, 0},
-			{0, 0, 0.5, 0.34},
-			{0, 0, 0, 0.33},
-		};
-
-		
-		//matrices for rewards upon transition
-		double rew_a1[][] = new double[][]{
-			{10, 0, 0, 0},
-			{5, 3, 0, 0},
-			{8, 0, 0, 0},
-			{5, 0, 0, 0},
-		};
-		double rew_a2_r[][] = new double[][]{
-			{5, 7, 10, 8},
-			{0, 2, 0, 0},
-			{0, 5, 7, 6},
-			{0, 2, 0, 2},
-		};
-		double rew_a2_k[][] = new double[][]{
-			{5, 7, 10, 8},
-			{0, 2, 0, 0},
-			{0, 5, 7, 6},
-			{0, 2, 2, 2},
-		};
-		double rew_a3[][] = new double[][]{
-			{3, 0, 5, 6},
-			{0, -1, 0, 0},
-			{0, 0, 1, 2},
-			{0, 0, 0, -1},
-		};
-
 		//Determine what state we are in
 		boolean enough = false;
 		boolean few = false;
 		boolean many_good = false;
 		boolean many_bad = false;
+		int s = 1; //value of current state: default to few cards
 		
 		//checks to see if enough train cards to buy a route
 		for (int i = 0; i < allRoutes.size(); i++) {
 			if(getNumTrainCardsByColor(allRoutes.get(i).getColor()) >= allRoutes.get(i).getCost()) {
 				enough = true;
+				s = 0;
 				break;
 			}
 		}
@@ -145,34 +147,44 @@ public class ManifestDestinyPlayer extends Player {
 		
 		//checks to see if many good
 		if(this.getHand().size() > 10) {
-			if(sum > 10)
+			if(sum > 10) {
 				many_good = true;
-			else
+				s = 2;
+			}
+			else {
 				many_bad = true;
+				s = 3;
+			}
 		}
 			
 		//checks to see if few cards
 		if(this.getHand().size() < 11) {
 			few = true;
+			s = 1;
 		}
 		
 		if(!enough && !many_good && !many_bad)
 			few = true;
 		
+		
+		//passing through to prob/reward function ---> just an example
+		int s_prime = 0;
+		probability(s, "a1", s_prime);
+		reward(s, "a1", s_prime);
 	
 		int future_index = 0;
 		
 		//evaluates between picking rainbow card, designated color card, or from deck
-		for (int s = 0; s < getFaceUpCards().size(); s++ ) {
-			if (getFaceUpCards().get(s).getColor().toString().equals("rainbow")) {
-				future_index = s+1;
+		for (int k = 0; k < getFaceUpCards().size(); k++ ) {
+			if (getFaceUpCards().get(k).getColor().toString().equals("rainbow")) {
+				future_index = k+1;
 			}
 		}
 		
 		if(future_index == 0) {
-			for( int s = 0; s < getFaceUpCards().size(); s++ ) {
-				if (cardColors.containsKey(getFaceUpCards().get(s).getColor())) {
-					future_index = s+1;
+			for( int k = 0; k < getFaceUpCards().size(); k++ ) {
+				if (cardColors.containsKey(getFaceUpCards().get(k).getColor())) {
+					future_index = k+1;
 				}
 			}
 		}
@@ -293,6 +305,87 @@ public class ManifestDestinyPlayer extends Player {
 		shortestPath.add(from); //list is reversed but we don't carrrre
 
 		return shortestPath;
+	}
+	
+	public double probability(int s, String a, int s_prime) {
+		
+		//STATES:					ACTIONS:
+		//0 = enough cards 			a1 = claim routes
+		//1 = few cards				a2_r = pick random card 
+		//2 = many good				a2_k = pick known card 
+		//3 = many bad				a3 = pick destination
+		
+		if(a.equals("a1")) {
+			return prob_a1[s][s_prime];
+		}
+		
+		else if(a.equals("a2_r")) {
+			return prob_a2_r[s][s_prime];
+		}
+		
+		else if(a.equals("a2_k")) {
+			return prob_a2_k[s][s_prime];
+		}
+		
+		else if(a.equals("a3")) {
+			return prob_a3[s][s_prime];
+		}
+		else 
+			return 0;
+	}
+	
+	public double reward(int s, String a, int s_prime) {
+		
+		//STATES:					ACTIONS:
+		//0 = enough cards 			a1 = claim routes
+		//1 = few cards				a2_r = pick random card 
+		//2 = many good				a2_k = pick known card 
+		//3 = many bad				a3 = pick destination
+		
+		if(a.equals("a1")) {
+			return rew_a1[s][s_prime];
+		}
+		
+		else if(a.equals("a2_r")) {
+			return rew_a2_r[s][s_prime];
+		}
+		
+		else if(a.equals("a2_k")) {
+			return rew_a2_k[s][s_prime];
+		}
+		
+		else if(a.equals("a3")) {
+			return rew_a3[s][s_prime];
+		}
+		else 
+			return 0;
+	}
+	public double value(int sprime) {
+		
+		return 0;
+	}
+	
+	public double quality(int s, String a) {
+		double action[][] = new double[4][4];
+		if(a.equals("a1")) {
+			action = prob_a1;
+		}
+		else if(a.equals("a2_k")) {
+			action = prob_a2_k;
+		}
+		else if(a.equals("a2_r")) {
+			action = prob_a2_r;
+		}
+		else if(a.equals("a3")) {
+			action = prob_a3;
+		}
+		double q = 0;
+		
+		for(int sp = 0; sp < 4; sp ++) {
+			q += probability(s, a, sp) * (reward(s, a, sp) + gamma*0);
+			
+		}
+		return 0;
 	}
 	
 }
